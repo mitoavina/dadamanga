a6f06cf74790194e703e9809519ce21ab5922889
KhaelM <mikerandria@gmail.com>
2019-12-29 09:33:56 -0500

    Download div added and factorized

---
+++ application/views/downloads.php
@@ -0,0 +1,28 @@
+<div class="position-relative downloads div3 up-footer text-white text-center container-fluid pt-5">
+    <div style="left:0;top:0;width: 100%;height:100%;position:absolute;background:rgba(183, 152, 109, .44)"></div>
+    <h5 class="mb-5 pb-5 pt-5">DOWNLOADS</h5>
+    <div class="row d-flex justify-content-center">
+        <div style="border: solid 1px white; border-radius: 10px;width:90%" class="pb-5 pt-5 row justify-content-center">
+            <div class="col-lg-2">
+                <img src="<?= base_url() ?>assets/img/followus/ring.png">
+                <div class="pt-2 font-weight-bold">Tipping & Food</div>
+            </div>
+            <div class="col-lg-2">
+                <img src="<?= base_url() ?>assets/img/followus/starfish.png">
+                <div class="pt-2 font-weight-bold">Animal Welfare</div>
+            </div>
+            <div class="col-lg-2">
+                <img src="<?= base_url() ?>assets/img/followus/file.png">
+                <div class="pt-2 font-weight-bold">Terms & Conditions</div>
+            </div>
+            <div class="col-lg-2">
+                <img src="<?= base_url() ?>assets/img/followus/smartphone.png">
+                <div class="pt-2 font-weight-bold">Mobile Phone Setup</div>
+            </div>
+            <div class="col-lg-2">
+                <img src="<?= base_url() ?>assets/img/followus/backpack.png">
+                <div class="pt-2 font-weight-bold">Preparation</div>
+            </div>
+        </div>
+    </div>
+</div>
