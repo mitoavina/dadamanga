ccedb350019c93da85094134695107fc46047e7f
dadamanga <dadamanga@me.com>
2020-01-06 19:14:04 +0000

    mail luxury

--- application/views/luxury_doc.php
+++ application/views/luxury_doc.php
@@ -1,44 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <title>reservation</title>
+<meta charset="utf-8">
+<title>reservation</title>
 </head>
+
 <body>
-    
-        <h1>Reservation Luxury</h1>
-        <h3>place : <?= $type ?></h3>
-        <p>reservation date : <?= $current ?> </p>
-        <hr>
-        <p class="phone"><?= $phone ?></p>
-        <h2>Client infos</h2>
-        <p><?= $name ?></p>
-        <p><?= $email ?></p>
-        <p class="n">Nationality:<?= $nationality ?></p>
-        <br>
-        <h2>travel infos</h2>
-        <p>arrive : <?= $begin ?></p>
-        <p>depart : <?= $end ?></p>
-        <p>disability/accessibility requirement  : <?= $disability ?></p>
-        <p >special food requirement  : <?= $food ?> </p>
-        <p>anything else  : <?= $else ?></p>
-        <div id="body">
-            
-        </div>
+
+<p id="logo" ><img width="150" src="<?= base_url("assets/img/svg/logo.svg") ?>" alt="dadamanga"></p>
+<h1 class="light" >Luxury Booking</h1>
+<h3 class="light case"><?= $name ?></h3>
+<h3 class="light">(<?= $current ?>)</h3>
+<hr>
+<h3>General informations</h3>
+<p>Full name : <?= $name ?></p>
+<p>Nationality: <?= $nationality ?></p>
+<p>Date of travel: <br>
+<p>arrive : <?= $begin ?></p>
+<p>depart : <?= $end ?></p>
+</p>
+<p>Food requirements: <br>
+<?= $food ?>
+</p>
+<p>Any disability / accessibility requirement? <br>
+<?= $disability ?>
+</p>
+<h3>Property</h3>
+<p><?= $type ?></p>
+<h3>Comments</h3>
+<p><?= $else ?></p>
 </body>
+
+
+
 </html>
 <style>
-    body{
-        font-family:'Arial'
-    }
-.n{
-    position:absolute;
-    top:230px;
-    right:53px;
+body {
+font-family: Helvetica, sans-serif;
+position:relative;
 }
-.phone{
-    position:absolute;
-    top:260px;
-    right:53px; 
+
+#logo{
+position: absolute;
+right:30;
+top:20px;
 }
+h3 {
+color: rgb(99, 99, 99);
+}
+
+h1 {
+color: rgb(22, 193, 236);
+}
+.light{
+font-weight: 300;
+}
+.case{
+text-transform: uppercase;
+}
+
 </style>
