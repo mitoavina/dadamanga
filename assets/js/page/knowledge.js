f20e990de9b36d00b55d44d4c99fad40b9c67d87
KhaelM <mikerandria@gmail.com>
2020-01-11 17:42:46 +0300

    knowledge-title dynamique

--- assets/js/page/knowledge.js
+++ assets/js/page/knowledge.js
@@ -1,1 +1,19 @@
-$(document).ready(function(){var e=$(`.knowledge-menu > li[data-index="${knowledge}"]`);$(e).addClass("side-menu-style-active");var a=$(`.dadamanga-special-card-container > div[data-index="${knowledge}"]`);a.removeClass("d-none"),$(a).css({position:"relative",left:"300px"}).show().animate({left:0},500),a.addClass("active");var n=$(e).siblings();$(n).each(function(){$(this).removeClass("side-menu-style-active")})});
+$(document).ready(function () {
+	var e = $(`.knowledge-menu > li[data-index="${knowledge}"]`);
+	$(e).addClass("side-menu-style-active");
+	var a = $(`.dadamanga-special-card-container > div[data-index="${knowledge}"]`);
+	a.removeClass("d-none"), $(a).css({
+		position: "relative",
+		left: "300px"
+	}).show().animate({
+		left: 0
+	}, 500), a.addClass("active");
+	var n = $(e).siblings();
+	$(n).each(function () {
+		$(this).removeClass("side-menu-style-active")
+    });
+    
+    $(".list-group-item > a").on("click", function(){
+        $(".knowledge-title").text($(this).text());
+    });
+});
