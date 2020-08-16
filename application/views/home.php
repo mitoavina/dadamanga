1c63589dc5699ff69ffeb7943678639cdedd26bb
KhaelM <michael.randrianarisona@outlook.com>
2020-08-16 18:09:47 +0300

    text added

--- application/views/home.php
+++ application/views/home.php
@@ -86,7 +86,7 @@
                 </div>
             </li>
             <li class="home-text jumbotron" style='background-image:url( <?= base_url("assets/img/home/adventure-jumbotron.jpg") ?>)'>
-                <h1 class="white-text">CREATE A BESPOKE<br> EXPERIENCE</h1>
+                <h1 class="white-text"> <?= $text[1]->value ?></h1>
                 <svg width="48" height="16" viewBox="0 0 48 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M47.7071 8.70711C48.0976 8.31658 48.0976 7.68342 47.7071 7.29289L41.3431 0.928932C40.9526 0.538408 40.3195 0.538408 39.9289 0.928932C39.5384 1.31946 39.5384 1.95262 39.9289 2.34315L45.5858 8L39.9289 13.6569C39.5384 14.0474 39.5384 14.6805 39.9289 15.0711C40.3195 15.4616 40.9526 15.4616 41.3431 15.0711L47.7071 8.70711ZM0 9L47 9V7L0 7L0 9Z" fill="white" />
                 </svg>
@@ -95,7 +95,7 @@
                 </div>
             </li>
             <li class="home-text jumbotron" style='background-image:url( <?= base_url("assets/img/home/knowledge.jpg") ?>)'>
-                <h1 class="white-text">KNOWLEDGE OF THE PLACE CONTENT HERE</h1>
+                <h1 class="white-text"> <?= $text[2]->value ?></h1>
                 <svg width="48" height="16" viewBox="0 0 48 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M47.7071 8.70711C48.0976 8.31658 48.0976 7.68342 47.7071 7.29289L41.3431 0.928932C40.9526 0.538408 40.3195 0.538408 39.9289 0.928932C39.5384 1.31946 39.5384 1.95262 39.9289 2.34315L45.5858 8L39.9289 13.6569C39.5384 14.0474 39.5384 14.6805 39.9289 15.0711C40.3195 15.4616 40.9526 15.4616 41.3431 15.0711L47.7071 8.70711ZM0 9L47 9V7L0 7L0 9Z" fill="white" />
                 </svg>
@@ -135,7 +135,7 @@
                             <span class="montserrat-bold">trip</span>
                         </p>
                         <p id="intro">
-                            Lorem ipsum dolor sit amet, cum at atqui partiendo, omnium scripserit vim an. Virtute nonumes vel ei, quis mundi sit te. Odio quot putent qui ei, labitur mediocrem in nec, sed cu wisi sensibus eleifend
+                        You can study key destinations and create a tour or, choose something “off the shelf”.  Have a look around, soak up the information, or, just send us a message and we will create your perfect tour.
                         </p>
                         <svg width="48" height="16" viewBox="0 0 48 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M47.7071 8.70711C48.0976 8.31658 48.0976 7.68342 47.7071 7.29289L41.3431 0.928932C40.9526 0.538408 40.3195 0.538408 39.9289 0.928932C39.5384 1.31946 39.5384 1.95262 39.9289 2.34315L45.5858 8L39.9289 13.6569C39.5384 14.0474 39.5384 14.6805 39.9289 15.0711C40.3195 15.4616 40.9526 15.4616 41.3431 15.0711L47.7071 8.70711ZM0 9L47 9V7L0 7L0 9Z" fill="#B7986D" />
@@ -217,8 +217,8 @@
             <p class="pb-5" id="madagascar_destination_specialist">MADAGASCAR <span class="montserrat-bold">Destination Specialist</span></p>
             <div class="big-text mx-1">
                 <p>
-                    WHY US? FIND OUT WHAT MAKES US DIFFERENT AND SPECIALISTS IN OUR FIELD.<br>
-                    OUR TAILOR MADE JOURNEYS ARE TO ESCAPE ON A WILDLIFE SAFARI AND THE ULTIMATE LUXURY ADVENTURE.</p>
+                    Why us? Find our what makes us different, understand why we are specialists in our field.  Our tailor-made journeys will provide a safe, well-managed escape on the ultimate adventure in a style that suits you and for a price you can understand.
+                </p>
                 <br>
                 <p>CONTACT US AND START PLANNING YOUR AFRICAN WILDERNESS JOURNEY TODAY.</p>
             </div>
