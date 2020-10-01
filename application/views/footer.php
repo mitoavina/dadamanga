7bd612b9bd96648ab0ccc3f11b04cea4dab211bc
KhaelM <michael.randrianarisona@outlook.com>
2020-10-01 19:59:11 +0300

    Adresse etc supprimé

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,17 +1,22 @@
 <footer class="page-footer text-white">
-    <div class="container">
-        <div class="row d-flex justify-content-center pt-3">
-            <img class="col-xs-2" src="<?= base_url() ?>assets/img/footer/footer_dm_logo.png">
-        </div>
-        <div class="row mb-5 d-flex justify-content-center">
-            <div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "about" ?>">About</a></div>
-            <div class="footer-link col-md-2 p-0 text-center text-lg-left"><a href="<?= base_url() . "destinations" ?>">Destinations</a></div>
-            <div class="footer-link col-md-2 p-0 text-center text-lg-left"><a href="<?= base_url() . "shelf" ?>">Off the Shelf</a></div>
-            <div class="footer-link col-md-2 p-0 text-center text-lg-left"><a href="<?= base_url() . "adventure" ?>">Create an Adventure</a></div>
-            <div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "luxury" ?>">Explore Luxury</a></div>
-            <div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "knowledge" ?>">Knowledge of the Place</a></div>
-        </div>
-        <div class="row">
+	<div class="container">
+		<div class="row d-flex justify-content-center pt-3">
+			<img class="col-xs-2" src="<?= base_url() ?>assets/img/footer/footer_dm_logo.png">
+		</div>
+		<div class="row mb-5 d-flex justify-content-center">
+			<div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "about" ?>">About</a></div>
+			<div class="footer-link col-md-2 p-0 text-center text-lg-left"><a
+					href="<?= base_url() . "destinations" ?>">Destinations</a></div>
+			<div class="footer-link col-md-2 p-0 text-center text-lg-left"><a href="<?= base_url() . "shelf" ?>">Off the
+					Shelf</a></div>
+			<div class="footer-link col-md-2 p-0 text-center text-lg-left"><a
+					href="<?= base_url() . "adventure" ?>">Create an Adventure</a></div>
+			<div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "luxury" ?>">Explore Luxury</a>
+			</div>
+			<div class="footer-link col-md-2 p-0 text-center"><a href="<?= base_url() . "knowledge" ?>">Knowledge of the
+					Place</a></div>
+		</div>
+		<!-- <div class="row">
             <div class="col-lg-4 text-lg-left text-center mb-2 mb-md-4">+261 34 20 258 62</div>
         </div>
         <div class="row">
@@ -26,33 +31,35 @@
                 Le Port Hotel, Avenue du Marechal Foch<br>
                 Isoraka Tolagnaro 614(BP 319)
             </div>
-        </div>
+        </div> -->
 
-        <div class="row mb-4 d-flex justify-content-center">
-            <div class="col-sm-6 col-md-2 text-center mb-4">
-                <img style="max-width: 100%;position: relative;top: 7px;width: 112px;right: 14px;" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_5.png">
-            </div>
-            <div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
-                <img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_1.png">
-            </div>
-            <div class="col-sm-6 col-md-2 pt-md-3 text-center mb-4 mr-4">
-                <img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_2.png">
-            </div>
-            <div class="col-sm-6 pt-md-4 col-md-2 text-center mb-4 mr-4">
-                <img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_3.png">
-            </div>
-            <div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
-                <img style="max-width:100%;width: 84px;position:relative;top: 1px;" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_4.png">
-            </div>
-            <div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
-                <img style="max-width:100%;" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_6.png">
-            </div>
-        </div>
+		<div class="row mb-4 d-flex justify-content-center">
+			<div class="col-sm-6 col-md-2 text-center mb-4">
+				<img style="max-width: 100%;position: relative;top: 7px;width: 112px;right: 14px;" alt=""
+					src="<?= base_url() ?>assets/img/footer/footer_logo_5.png">
+			</div>
+			<div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
+				<img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_1.png">
+			</div>
+			<div class="col-sm-6 col-md-2 pt-md-3 text-center mb-4 mr-4">
+				<img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_2.png">
+			</div>
+			<div class="col-sm-6 pt-md-4 col-md-2 text-center mb-4 mr-4">
+				<img style="max-width: 100%" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_3.png">
+			</div>
+			<div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
+				<img style="max-width:100%;width: 84px;position:relative;top: 1px;" alt=""
+					src="<?= base_url() ?>assets/img/footer/footer_logo_4.png">
+			</div>
+			<div class="col-sm-6 col-md-2 text-center mb-4 mr-4">
+				<img style="max-width:100%;" alt="" src="<?= base_url() ?>assets/img/footer/footer_logo_6.png">
+			</div>
+		</div>
 
-        <div class="row d-flex justify-content-center text-center">
-            <div class="mr-4"><small class="">&copy; Dadamanga 2019</small></div>
-            <!-- <div class="mr-4"><small>Terms & Conditions</small></div>
+		<div class="row d-flex justify-content-center text-center">
+			<div class="mr-4"><small class="">&copy; Dadamanga 2019</small></div>
+			<!-- <div class="mr-4"><small>Terms & Conditions</small></div>
             <div class=""><small>Privacy Policy</small></div> -->
-        </div>
-    </div>
+		</div>
+	</div>
 </footer>
