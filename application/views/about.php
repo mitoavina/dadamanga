0c4fd1bccd9edc9c2fd4130a808423bf782d83c7
KhaelM <michael.randrianarisona@outlook.com>
2020-10-02 23:46:43 +0300

    about.css version updated

--- application/views/about.php
+++ application/views/about.php
@@ -11,7 +11,7 @@
 	<link href="<?= base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
 	<!-- Custom CSS -->
 	<link href="<?= base_url() ?>assets/css/global.css?v=0.0.1" rel="stylesheet">
-	<link href="<?= base_url() ?>assets/css/page/about.css" rel="stylesheet">
+	<link href="<?= base_url() ?>assets/css/page/about.css?v=0.0.1" rel="stylesheet">
 	<!-- Material Design Bootstrap -->
 	<link href="<?= base_url() ?>assets/css/mdb.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
