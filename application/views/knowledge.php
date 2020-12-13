1f27c6f638db9e9240626d5c4f6dcb33eaddfe99
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2020-12-13 18:24:38 +0300

    content added

--- application/views/knowledge.php
+++ application/views/knowledge.php
@@ -105,15 +105,21 @@
 						<div class="dadamanga-special-card-container knowledge-details col-lg-8 col-sm-8 col-xs-12">
 							<div data-index="madagascar" class="row d-none">
 								<div class="knowledge col-lg-6 col-sm-12">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor
-										incididunt ut labore etdolore magna aliqua.. Ut enim ad minimeiusmod tempor
-										incididunt ut labore etdolore ma</p>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor
-										incididunt ut labore etdolore magna aliqua.. Ut enim ad minimeiusmod tempor
-										incididunt ut labore etdolore ma</p>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor
-										incididunt ut labore etdolore magna aliqua.. Ut enim ad minimeiusmod tempor
-										incididunt ut labore etdolore ma</p>
+									<p>Madagascar is the largest island in the Indian Ocean, and it is famous for its unique wildlife and
+biodiversity. With unique flora and fauna, white sandy beaches, beautiful tropical forests,
+delicious local cuisine and a warm and welcoming population, this place offers an unforgettable
+experience in a lifetime. Country of "mora-mora" (translates gently-gently), the nation and its
+people are indeed distinguished by their sweetness of life.</p>
+									<p>Despite its largely westernized culture in the largest cities of the country, the Malagasy people
+indeed have a way of enjoying life, and they willingly share it with visitors. Thus, discovering
+this country cannot be done only by exploring its tourist sites. Traveling to Madagascar also
+means adapting and enjoying a lifestyle conducive to well-being and relaxation.</p>
+									<p>Malagasy people are also great revelers, exceptional artists, and experts in enjoying the sweetness
+of life, and discovering the many entertainments associated with these personality traits is an
+experience not to be missed. Through the different populations of the big island, a trip to
+Madagascar will thus be the discovery of a wide variety of culture influenced by the African,
+Asian and European ancestry of its people. This cosmopolitan mixture associated with the
+marvelous tourist sites makes all the charm of the country</p>
 								</div>
 								<div class="knowledge col-lg-6 col-sm-12">
 									<img src="<?= base_url() ?>/assets/img/knowledge/madagascar.png" alt="" width="100%"
@@ -124,14 +130,17 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>The baobab is an iconic emblem of the Malagasy flora. This majestic tree is one of Madagascar's
+greatest treasures, as six of the eight species of baobab that exist in the world are endemic to the
+Red Island. Also called Reniala (mother of the forest), baobabs are not just beautiful to admire,
+they also produce fruit, and are important in the ecosystems to which they belong.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>While baobabs can be seen from north to south Madagascar, some stand out more than others.
+The most famous are undoubtedly those of the alley of the baobabs in Morondava. Emblematic
+landscape of the country, it is one of Madagascar's best-known tourist showcases. We can also
+mention the baobabs of the spiny forest south of Fort Dauphin, from Ifaty, Zombitse,
+Ankarafantsika and Kirindy nature reserves.</p>
 										</div>
 									</div>
 								</div>
@@ -159,14 +168,17 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>These geological curiosities are craggy limestone points resulting from phenomena that occurred
+millions of years ago. Offering a unique and astonishing landscape, these rock formations are
+present in several national parks of Madagascar. The most famous of the karst formations in the
+country is Tsingy de Bemaraha, in the west of the big island.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Listed as a UNESCO World Heritage Site, admiring the landscape of this nature reserve perched
+on a suspension bridge is an extraordinary experience. Note that the majority of Tsingy are found
+in the western regions of the country, and among them we can cite the Red Tsingy of Diego and
+Ankarana. The first is easy to access, and just as magnificent as the second which stands out for
+its rich ecological and cultural heritage linked to the Antakarana people.</p>
 										</div>
 									</div>
 								</div>
@@ -194,14 +206,17 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Madagascar's beaches easily compete with the most beautiful beaches in the world. But the
+difference on the Red Island is that the majority of its resorts are untouched by mass tourism, and
+tranquility and relaxation are always there. Being an island, and having other small islands, it is
+surely not the beaches that are lacking in Madagascar.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Water sports and other seaside activities - such as diving or whale watching - are other reasons to
+enjoy the turquoise waters of the Big Island. Among the unmissable beaches of Madagascar, we
+can first mention those of the islands of Nosy Be and Sainte Marie, without forgetting that Fort
+Dauphin is surrounded by endless white-sand beaches in every direction. The most famous
+beaches in the country are all close to all kinds of hotel facilities to enjoy a dream vacation.</p>
 										</div>
 									</div>
 								</div>
@@ -239,14 +254,19 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Madagascar is a place that arouses curiosity. Nature has evolved at its own pace and according to
+its own rules. As proof, about 80% of all animal and plant species found in Madagascar are 
+endemic. This is because the island has been isolated for over 80 million years. Among the
+endemic species found only in the big island, we can cite the lemurs, the Fosa as well as six
+species of baobab. Because of its unique biodiversity - on land, in the air and in the water -
+Madagascar is often referred to as "the eighth continent”.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Unfortunately, Malagasy biodiversity is greatly threatened by human activity. Despite the many
+conservation programs in place in the country, many species are currently endangered in
+Madagascar. For the most intrepid and courageous of tourists, it is possible to participate in the
+safeguarding of Madagascan nature during stays in the country. Many NGOs and other
+environmental activist organizations welcome any help we can give them with open arms.</p>
 										</div>
 									</div>
 								</div>
@@ -279,14 +299,27 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>The culture of the Malagasy people reflects their origins in Southeast Asia and East Africa. The
+influence of Arabs, Indians, British, French and Chinese settlers is also evident. Even if the
+Malagasy share a common culture throughout the big island, each people - depending on its
+region - has its own habits and customs. Sometimes a source of tension, these cultural differences
+are nevertheless all the charm of the Malagasy population. It is thanks to them that one can
+discover so much different in the many towns and villages of the island.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>If life is already very westernized in the largest cities of Madagascar, the vestiges of the country's
+royal and colonial past can still be admired in many tourist sites. Regarding the villages far from
+the big cities, these are open-air exhibitions of the ancestral and traditional way of life of some
+Malagasy people. Among the most notable cultural practices in the country, we can cite the
+“famadihana” or the turning of the dead.</p>
+										</div>
+										<div class="col-lg-6">
+											<p>
+											Finally, Malagasy culture is inseparable from its gastronomy, music and crafts. These are largely
+inspired by the country's natural resources. As for music and literary art, it is expressed in the
+local language: Malagasy. Reflecting Malagasy culture as a whole, the language spoken in
+Madagascar is the same throughout the island, but also varies in various dialects.
+											</p>
 										</div>
 									</div>
 								</div>
@@ -314,14 +347,20 @@
 								<div class="knowledge col-lg-12">
 									<div class="row">
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>Madagascar is a destination of choice for water sports enthusiasts of all kinds. With its many
+beaches and other coastal sites, you can practice sports such as windsurfing and kitesurfing,
+fishing and diving. Over the years, land sport has also been greatly enriched in the country. If
+hiking and trekking are the sports most practiced by tourists, private companies now offer
+activities such as parachute jumping, paragliding, rapid descent or even mountain biking or
+motocross treks.</p>
 										</div>
 										<div class="col-lg-6">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod
-												tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad
-												minimeiusmod tempor incididunt ut labore etdolore ma</p>
+											<p>As for more traditional sports, be aware that pétanque is the country's national sport, and a stay in
+Madagascar is the perfect opportunity to improve skills in the matter. In addition, large cities
+have sports halls and other sports clubs where it is possible to train. Whether it is tennis, golf,
+basketball, martial arts or football, the majority of sports are practicable in the country. Finally,
+when we talk about sport in Madagascar, it is important to talk about Moraingy, the traditional
+martial art of the country</p>
 										</div>
 									</div>
 								</div>
--- application/views/knowledges.php
+++ application/views/knowledges.php
@@ -45,17 +45,25 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2 pt-5 pb-5 text_header">
                     <p class="text-center">
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do euismod tempor incididunt ut labore et dolore aliqua.. Ut enim ad minim veniam, quis nostrud exercitation ullamo laboris nisi ut aliquip ex ea
+                    With its iconic lemurs, its paradisiacal beaches, its impenetrable jungles, its breathtaking
+landscapes and its fascinating underwater life; Madagascar is an irresistible destination for
+adventurers and lovers of ecological and seaside tourism. A thousand and one wonders are to be
+discovered on the Red Island, and a trip to Madagascar is a unique experience that will
+undoubtedly be rich in discovery, pleasure and emotion.
                     </p>
                 </div>
             </div>
             <div class="container pb-4 mt-3 mb-5">
                 <div class="row">
                     <div class="col-md-6 text_secondaire">
-                        <p>« Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad minimeiusmod tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad minim</p>
+                        <p>Visiting Madagascar, however, can not be easy, and using the services of a travel agency and tour
+guides are strongly recommended. To give you a taste of a stay on the Big Island, we offer
+descriptions of the top attractions of the country and each of its regions.</p>
                     </div>
                     <div class="col-md-6 text_secondaire">
-                        <p>« Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad minimeiusmod tempor incididunt ut labore etdolore magna aliqua.. Ut enim ad minim</p>
+                        <p>We are therefore at your disposal to concoct tailor-made trips according to your preferences and
+your budget. Whether it is an ecological and cultural discovery of the island with ease, or a
+luxury stay in the best seaside resorts in the country, our team is at your entire disposal.</p>
                     </div>
                 </div>
             </div>
