39c2aab76d8912ec63b3690318e62d9c6eb3735e
michael-openflex <michael.openflex@gmail.com>
2021-01-25 07:27:01 +0300

    blur

--- application/views/pages/off-the-shelf.php
+++ application/views/pages/off-the-shelf.php
@@ -71,7 +71,7 @@
 
                         <!-- Modal -->
                         <div class="modal fade" id="trip<?= $trip->getId() ?>" tabindex="-1" role="dialog" aria-hidden="false">
-                            <div class="modal-dialog modal-lg" role="document">
+                            <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <h5 class="modal-title"><?= $trip->getName() ?></h5>
@@ -84,7 +84,7 @@
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary" data-dismiss="modal">Add To Cart</button>
+                                        <button type="button" class="btn btn-primary" data-dismiss="modal">Add To Cart <i class="fas fa-shopping-cart"></i></button>
                                     </div>
                                 </div>
                             </div>
