dab2b0ba6ed6d345f5c97292b9b45eb580ceb9c4
michael-openflex <michael.openflex@gmail.com>
2021-03-03 15:53:11 +0300

    posi to post

--- application/views/components/modal_msg.php
+++ application/views/components/modal_msg.php
@@ -8,7 +8,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p>We are currently making exciting new changes to our Internet presence and the functionality of our website, and creating amazing new posi COVID products for your coming safari to our wonderful country.</p>
+                <p>We are currently making exciting new changes to our Internet presence and the functionality of our website, and creating amazing new post COVID products for your coming safari to our wonderful country.</p>
                 <p>Please note that the website is not fully functional, but we aim for it to be up and running by the end of March 2021.</p>
                 <p>Please CLICK ON 'close' to enter the website and have a look around while you wait for us finish our construction work.</p>
                 <p>or CONTACT us...</p>
