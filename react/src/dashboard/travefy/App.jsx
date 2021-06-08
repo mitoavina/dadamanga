286b41f6861b0d73725dfa30ace47f359ef48392
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2021-06-08 09:42:00 +0300

    Trip: Langage, prix modifiable dans le backend

--- react/src/dashboard/travefy/App.jsx
+++ react/src/dashboard/travefy/App.jsx
@@ -1,13 +1,60 @@
+const apiRequest = url => fetch(url).then(response => response.json());
+const convertTravefyForApi = (travefy) => {
+    return {
+        tripId: travefy.travefy_trip_id,
+        tripPrice: Number.parseInt(travefy.dm_trip_price),
+        languageId: Number.parseInt(travefy.dm_language_id)
+    }
+}
+
 const App = () => {
     const [state, setState] = React.useState({
         travefyIds: [],
-        newTravefyTripId: ""
+        newTravefyTripId: "",
+        languages: []
     })
 
-    const { travefyIds, newTravefyTripId } = state;
+    const { travefyIds, newTravefyTripId, languages } = state;
+
+    const updateAsyncImgAndTitleTravefy = (index, trip) => {
+        fetch(`https://frozen-falls-91201.herokuapp.com/https://trips.dadamanga.mg/itinerary-rest-api/itineraryPages/${trip.travefy_trip_id}`).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("This trip doesn't exist");
+            }
+        }).then(results => {
+            setState(prevState => {
+                return {
+                    ...prevState,
+                    travefyIds: [...prevState.travefyIds.slice(0, index), { ...prevState.travefyIds[index], img: results.itineraryPage.trip.mediaImages[0].url, title: results.itineraryPage.trip.name }, ...prevState.travefyIds.slice(index + 1),]
+                }
+            })
+        }).catch(err => {
+            setState(prevState => {
+                return {
+                    ...prevState,
+                    travefyIds: [...prevState.travefyIds.slice(0, index), { ...prevState.travefyIds[index], img: null, title: null }, ...prevState.travefyIds.slice(index + 1),]
+                }
+            })
+        });
+    }
+
+    const toggleTravefy = (trip, index) => {
+        if (trip.dm_trip_price === null) {
+            trip.dm_trip_price = 0;
+        } else {
+            trip.dm_trip_price = null;
+        }
+
+        setState({
+            ...state,
+            travefyIds: [...travefyIds.slice(0, index), trip, ...travefyIds.slice(index + 1)]
+        });
+    }
 
     const getAllTravefyTrip = () => {
-        fetch(`${SERVER_URL}/WebServices/getAllTravefyTrip`, {
+        return fetch(`${SERVER_URL}/WebServices/getAllTravefyTrip`, {
             method: 'GET',
         }).then(res => {
             if (!res.ok) {
@@ -15,28 +62,60 @@ const App = () => {
             }
             return res.json();
         }).then(json => {
-            console.log(json);
-            setState({ ...state, travefyIds: json.data });
         }).catch(err => {
             console.log('caught it!', err);
         });
     }
 
+    const apiRequestAllLanguages = () => {
+        return apiRequest(`${SERVER_URL}/WebServices/getAllLanguages`).then(results => {
+            return results.data.map(language => {
+                return {
+                    id: language.id,
+                    language: language.dm_language
+                };
+            });
+        });
+    }
+
+    const apiRequestAllTravefyTrip = () => {
+        return apiRequest(`${SERVER_URL}/WebServices/getAllTravefyTrip`).then(results => {
+            return results.data.map(trip => {
+                return trip
+            });
+        });
+    }
+
     React.useEffect(() => {
-        getAllTravefyTrip();
+        Promise.all([
+            apiRequestAllLanguages(),
+            apiRequestAllTravefyTrip()
+        ]).then(values => {
+            setState({
+                ...state,
+                languages: values[0],
+                travefyIds: values[1]
+            })
+            for (let index = 0; index < values[1].length; index++) {
+                updateAsyncImgAndTitleTravefy(index, values[1][index]);
+            }
+        }).catch(err => {
+            console.log("Oops, smth went wrong", err);
+        })
     }, [])
 
 
-    const patchTravefyTrip = (id, newTravefyTripId) => {
+    const patchTravefyTrip = (id, travefyId, index) => {
+        console.log("travefyId", travefyId, convertTravefyForApi(travefyId));
         fetch(`${SERVER_URL}/WebServices/updateTravefyTripId/${id}`, {
             method: 'PATCH',
-            body: JSON.stringify({ tripId: newTravefyTripId })
+            body: JSON.stringify(convertTravefyForApi(travefyId))
         }).then(res => {
             if (!res.ok) {
                 res.text().then(text => { throw Error(text) });
             } else {
                 alert('Update succeeded!');
-                getAllTravefyTrip();
+                updateAsyncImgAndTitleTravefy(index, travefyId);
             }
         }).catch(err => {
             console.log('caught it!', err);
@@ -86,6 +165,24 @@ const App = () => {
         });
     }
 
+    console.log("state", state);
+
+    const renderImgAndTitle = (travefyId) => {
+        if (travefyId.title === undefined) {
+            return "Loading...";
+        } else {
+            if (travefyId.title !== null) {
+                return (<div className="card">
+                    <img src={travefyId.img} alt="Loading" />
+                    {travefyId.title}
+                </div>)
+            } else {
+                return ("Trip  doesn't exist in your travefy account");
+            }
+        }
+
+    }
+
     return (
         <div>
             <input value={newTravefyTripId} onChange={(e) => {
@@ -100,6 +197,9 @@ const App = () => {
                 <thead>
                     <tr>
                         <th>Travefy Id</th>
+                        <th>Price</th>
+                        <th>Language</th>
+                        <th>Info</th>
                         <th></th>
                         <th></th>
                     </tr>
@@ -108,14 +208,38 @@ const App = () => {
                     {travefyIds.map((travefyId, index) => {
                         return (
                             <tr key={index}>
-                                <td><input onChange={(e) => {
+                                <td><input className="travefy-id" onChange={(e) => {
                                     setState({
                                         ...state,
                                         travefyIds: [...travefyIds.slice(0, index), { ...travefyId, travefy_trip_id: e.currentTarget.value }, ...travefyIds.slice(index + 1)]
                                     })
                                 }} value={travefyId.travefy_trip_id} /></td>
+                                <td>
+                                    <div><input type="checkbox" onChange={() => { toggleTravefy(travefyId, index) }} checked={travefyId.dm_trip_price === null ? true : false} value="Manual" />From Travefy</div>
+                                    <div><input type="number" value={travefyId.dm_trip_price ? travefyId.dm_trip_price : ''} disabled={travefyId.dm_trip_price === null ? true : false} onChange={(e) => {
+                                        setState({
+                                            ...state,
+                                            travefyIds: [...travefyIds.slice(0, index), { ...travefyId, dm_trip_price: e.currentTarget.value }, ...travefyIds.slice(index + 1)]
+                                        })
+                                    }} /></div>
+                                </td>
+                                <td>
+                                    <select value={travefyId.dm_language_id} onChange={(e) => {
+                                        setState({
+                                            ...state,
+                                            travefyIds: [...travefyIds.slice(0, index), { ...travefyId, dm_language_id: e.currentTarget.value }, ...travefyIds.slice(index + 1)]
+                                        })
+                                    }}>
+                                        {languages.map(language => {
+                                            return (<option key={language.id} value={language.id} >{language.language}</option>)
+                                        })}
+                                    </select>
+                                </td>
+                                <td>
+                                    {renderImgAndTitle(travefyId)}
+                                </td>
                                 <td><button onClick={() => {
-                                    patchTravefyTrip(travefyId.id, travefyId.travefy_trip_id);
+                                    patchTravefyTrip(travefyId.id, travefyId, index);
                                 }}>Update</button></td>
                                 <td><button onClick={() => {
                                     removeTravefyId(travefyId.id);
