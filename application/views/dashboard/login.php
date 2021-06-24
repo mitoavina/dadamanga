7bee11fe5aa76255f6f96433dc9dde1fd4e23d8d
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2021-06-24 23:02:16 +0300

    Mot de passe oublié effacé

--- application/views/dashboard/login.php
+++ application/views/dashboard/login.php
@@ -62,14 +62,6 @@
 							Log in
 						</button>
 					</div>
-					<div class="text-center p-t-12">
-						<span class="txt1">
-							Forgot
-						</span>
-						<a class="txt2" href="#">
-							Username / Password?
-						</a>
-					</div>
 				</form>
 			</div>
 		</div>
