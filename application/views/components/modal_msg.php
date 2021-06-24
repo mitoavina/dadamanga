a30f570d5ebdd99a4c26bf674e573bcdea31cabc
Michael Randrianarisona <michael.randrianarisona@outlook.com>
2021-06-24 23:03:21 +0300

    Modal fin mars vers soon

--- application/views/components/modal_msg.php
+++ application/views/components/modal_msg.php
@@ -9,7 +9,7 @@
             </div>
             <div class="modal-body dadamanga">
                 <p>We are currently making exciting new changes to our Internet presence and the functionality of our website, and creating amazing new post COVID products for your coming safari to our wonderful country.</p>
-                <p>Please note that the website is not fully functional, but we aim for it to be up and running by the end of March 2021.</p>
+                <p>Please note that the website is not fully functional, but we aim for it to be up and running soon.</p>
                 <p>Please CLICK ON 'close' to enter the website and have a look around while you wait for us finish our construction work.</p>
                 <p>or CONTACT us...</p>
             </div>
